e19e826fc4e372c89880c4b4af23997cc21bf092
Chandler Hayes <cah468@nau.edu>
2017-03-07 15:31:35 -0700

    Created the about page and added additional buttons to the home page

---
+++ MyTab/app/src/main/java/com/example/chandler/hack/wine_list.java
@@ -0,0 +1,14 @@
+package com.example.chandler.hack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class wine_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wine_list);
+    }
+}
